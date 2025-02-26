4d4955ab8bc249e1d485d8311c9069e707755de3
Amir <amir.batyrggaliev@gmail.com>
2025-02-27 00:11:24 +0500

    Divide mainDisplay to two separate first, second

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const buttonContainer = document.querySelector(".btn-container");
 const numberButtons = buttonContainer.querySelectorAll(".number");
 const operatorButtons = buttonContainer.querySelectorAll(".operator");
-const mainDisplay = document.querySelector("#display");
+const displayFirstOperand = document.querySelector(".first-operand");
+const displaySecondtOperand = document.querySelector(".second-operand");
 const equalsButton = document.querySelector("#equal");
 
 let operator = null;
