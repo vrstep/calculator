3f435b6dc8f61f0d4b885b2c9979f5ca2d785fba
Amir <amir.batyrggaliev@gmail.com>
2025-04-01 23:51:22 +0500

    Clear expression display when = clicked

--- script.js
+++ script.js
@@ -3,75 +3,83 @@ const numberButtons = buttonContainer.querySelectorAll(".number");
 const operatorButtons = buttonContainer.querySelectorAll(".operator");
 const displayFirstOperand = document.querySelector(".first-operand");
 const displaySecondOperand = document.querySelector(".second-operand");
+const displayCalculation = document.querySelector("#display-calculation");
 const equalsButton = document.querySelector("#equal");
 
 let operator = null;
 let firstOperand = null;
 let secondOperand = "";
 
-numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        getNum(e.target.textContent);
-    });
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    getNum(e.target.textContent);
+  });
 });
 
 operatorButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener("click", (e) => {
-        getOp(e.target.textContent);
-    });
+  operatorButton.addEventListener("click", (e) => {
+    getOp(e.target.textContent);
+  });
 });
 
 function getNum(num) {
-    secondOperand += num;
-    secondOperand = parseFloat(secondOperand);
-    displaySecondOperand.textContent = secondOperand;   
+  secondOperand += num;
+  secondOperand = parseFloat(secondOperand);
+  displaySecondOperand.textContent = secondOperand;
 }
 
 function getOp(op) {
-    if (operator === null) {
-        firstOperand = secondOperand;
-    } else if (firstOperand !== null) {
-        firstOperand = operate(firstOperand, secondOperand, operator);
-    }
+  if (operator === null) {
+    firstOperand = secondOperand;
+  } else if (firstOperand !== null) {
+    firstOperand = operate(firstOperand, secondOperand, operator);
+  }
 
-    displayFirstOperand.textContent = firstOperand + " " + op;
-    operator = op;
-    secondOperand = "";
-    displaySecondOperand.innerHTML = "0";
+  displayFirstOperand.textContent = firstOperand + " " + op;
+  operator = op;
+  secondOperand = "";
+  displaySecondOperand.innerHTML = "0";
 }
 
 equalsButton.addEventListener("click", () => {
-    console.log(operate(firstOperand, secondOperand, operator));
+  displayCalculation.textContent = operate(
+    firstOperand,
+    secondOperand,
+    operator
+  );
+  displayFirstOperand.textContent = "";
+  displaySecondOperand.textContent = "";
+  console.log(operate(firstOperand, secondOperand, operator));
 });
 
 function operate(firstOperand, secondOperand, operator) {
-    switch (operator) {
-        case "+":
-            return add(firstOperand, secondOperand);
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
 
-        case "-":
-            return substract(firstOperand, secondOperand);
+    case "-":
+      return substract(firstOperand, secondOperand);
 
-        case "*":
-            return multiply(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
 
-        case "/":
-            return divide(firstOperand, secondOperand);
-    }
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
 }
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function substract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
